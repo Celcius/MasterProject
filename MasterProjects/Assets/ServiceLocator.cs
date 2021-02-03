4ddb5fdaf528c4fd69fed1b2606d927631fee154
zoreander <ivo.capelo@gmail.com>
2021-02-03 09:58:14 +0000

    Migrated Grid Tools from Goblin Ghost

---
+++ MasterProjects/Assets/ServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AmoaebaUtils;
+
+public class ServiceLocator : SingletonScriptableObject<ServiceLocator>
+{
+    [SerializeField]
+    private CameraMoverVar cameraMover;
+    public CameraMover CameraMover => cameraMover.Value;
+
+    [SerializeField]
+    private GridMonoBehaviourVar cameraFocusVar;
+    public GridMonoBehaviour CameraFocus => cameraFocusVar.Value;
+}
