3dfc4bd3dbce27399611fb0bec127eb438a447fc
zoreander <ivo.capelo@gmail.com>
2021-02-18 16:47:52 +0000

    Double Items (Grass + Boulder || Tree, Crack + Boulder || Tree)

---
+++ MasterProjects/Assets/Scripts/AmoaebaTileTools/MultiTileEntity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTileEntity : GridEntity
+{
+    [SerializeField]
+    private Vector3Int[] occupyOffsets;
+
+    public override bool Occupies(Vector3Int gridPos)
+    {
+        if(!gameObject.activeInHierarchy)
+        {
+            return false;
+        }
+
+        if(this.GridPos == gridPos)
+        {
+            return true;
+        }
+
+        if(occupyOffsets == null || occupyOffsets.Length == 0)
+        {
+            return false;
+        }
+
+        foreach(Vector3Int offset in occupyOffsets)
+        {
+            Vector3Int pos = this.GridPos + offset;
+            if(gridPos == pos)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ MasterProjects/Assets/Scripts/AmoaebaTileTools/SpawnRuleTile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using AmoaebaUtils;
+
+public class SpawnRuleTile : RuleTile<Tile>
+{
+
+        [SerializeField]
+        public GameObject toInstantiate;
+
+        [SerializeField]
+        private TilemapVar tilemapVar;
+
+        private GameObject instantiated = null;
+
+        public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+        {
+            base.RefreshTile(position, tilemap);
+        }
+
+        public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+        {   
+           bool startup = base.StartUp(position, tilemap, go);
+
+           ReInstantiate(position, tilemap);
+        
+           return startup;
+        }
+
+        private void ReInstantiate(Vector3Int position, ITilemap tilemap)
+        {
+            if(!Application.isPlaying)
+            {
+                return;
+            }
+            if(instantiated != null)
+            {
+                Destroy(instantiated);
+            }
+
+            Vector3 cellSize = new Vector3(tilemap.localBounds.size.x / tilemap.size.x,
+                                           tilemap.localBounds.size.y / tilemap.size.y,
+                                           tilemap.localBounds.size.z / tilemap.size.z);   
+            
+
+            Vector3 pos  = Vector3.Scale(position, cellSize)  + cellSize/2.0f;
+            
+            Instantiate(toInstantiate, 
+                        pos,
+                        Quaternion.identity, 
+                        tilemapVar.Value.gameObject.transform);
+        }
+
+}
