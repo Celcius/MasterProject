5e558d5ae220dce6ab80cc3a20dea2ec596255f9
zoreander <ivo.capelo@gmail.com>
2021-03-26 17:55:17 +0000

    new ghost grandma anim

---
+++ MasterProjects/Assets/TriggerAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : RoomChangeHandler
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private string animTrigger;
+
+    [SerializeField]
+    private string cancelAnimTrigger;
+
+    private bool hasTriggered = false;
+
+    
+    public override void OnRoomEnter(Vector2Int pos)
+    {
+
+    }
+
+    public override void OnRoomLeave(Vector2Int pos)
+    {
+        if(CameraMover.RoomPosForWorldPos(transform.position) == pos)
+        {
+            animator.SetTrigger(cancelAnimTrigger);
+            hasTriggered = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        if(!hasTriggered && other.tag == GameConstants.PLAYER_TAG)
+        {
+            hasTriggered = true;
+            animator.SetTrigger(animTrigger);
+        }
+    }
+
+}
