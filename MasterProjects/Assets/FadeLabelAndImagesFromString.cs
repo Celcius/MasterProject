a2c8e2941c431493f466bfa21f1f0aafade49294
zoreander <ivo.capelo@gmail.com>
2021-03-30 18:13:57 +0100

    New Height Banner

---
+++ MasterProjects/Assets/FadeLabelAndImagesFromString.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AmoaebaUtils;
+
+public class FadeLabelAndImagesFromString : FadeLabelFromStringVar
+{
+    [SerializeField]
+    private Image[] images;
+
+    protected override IEnumerator FadeTo(float goal)
+    {
+        float delta = (goal - label.alpha) / fadeInTime;
+        float elapsed = 0;
+        float alpha = label.alpha;
+        Color c = label.color;
+        while(elapsed <= fadeInTime)
+        {
+            yield return new WaitForEndOfFrame();
+            alpha += Time.deltaTime * delta;
+            elapsed += Time.deltaTime;
+            c.a = alpha;
+            label.color = c;
+            foreach(Image image in images)
+            {
+                Color iColor = image.color;
+                iColor.a = c.a;
+                image.color = iColor;
+            }
+        }
+        c.a = goal;
+    }
+}
